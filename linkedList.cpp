df78429d1492261fd4dddf4399c39af8fe5f92d2
Ganesh Dagadi <73548263+ganesh-dagadi@users.noreply.github.com>
2022-11-09 21:09:51 +0530

    Completed till linkedlist reverse

--- linkedList.cpp
+++ linkedList.cpp
@@ -28,19 +28,58 @@ int main(){
         }
 
         void printElements(){
-            cout << this->data << endl;
+            cout << this->data << ">>";
             node* temp = this;
             while(temp->link != NULL){
                 temp = temp->link;
-                cout << temp->data << endl;
-            }       
+                cout << temp->data << ">>";
+            }
+            cout << endl;   
+        }
+
+        void deleteHead(node* &head){
+            node * toDel = head;
+            head = head->link;
+            delete toDel;
+        }
+        void deleteVal(int val){
+            node* temp = this;
+            while(temp->link->data != val){
+                temp=temp->link;
+            }
+            node* toDel = temp->link;
+            temp->link = temp ->link -> link;
+            delete toDel;
+        }
+
+        void reverseIter(node* &head){
+            node* prev = NULL;
+            node* curr = head;
+            node* next = head->link;
+            while(next->link != NULL){
+                curr->link = prev;
+                prev = curr;
+                curr = next;
+                next = next->link;
+            }
+            head = next;
+            curr->link = prev;
+            head->link = curr;
         }
     };
 
     node* linkedList = new node(5);
     linkedList->insertAtEnd(10);
     linkedList->insertAtEnd(20);
+    linkedList->insertAtEnd(20);
+    linkedList->insertAtEnd(30);
     linkedList->insertBeginning(linkedList , 40);
+    linkedList->printElements();
+    // linkedList->deleteHead(linkedList);
+    // cout << "After delete" << endl;
+    // linkedList->printElements();
+    linkedList->reverseIter(linkedList);
+    cout << "After reverse \n" << endl;
     linkedList->printElements();
     return 0;
 }
